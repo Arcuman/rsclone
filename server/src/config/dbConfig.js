85e82dc40020a01c6730a63860ff6ee66b6ca297
Arcuman <46977740+Arcuman@users.noreply.github.com>
2021-01-05 14:02:10 +0300

    delete js files

--- server/src/config/dbConfig.js
+++
@@ -1,7 +0,0 @@
-import { DB_USER, DB_NAME, DB_PASSWORD, DB_PORT } from "../constants/constants";
-export const config = {
-    user: DB_USER,
-    database: DB_NAME,
-    password: DB_PASSWORD,
-    port: DB_PORT,
-};
