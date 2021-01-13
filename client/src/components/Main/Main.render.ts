cb59d689b4c0077e571c7754eb09160929b6ecc4
Ilya-Baklanov <yahters4@gmail.com>
2021-01-14 00:35:34 +0400

    feat: add router

--- client/src/components/Main/Main.render.ts
+++ client/src/components/Main/Main.render.ts
@@ -1,9 +1,20 @@
 import { createHtmlElement } from '@/utils/utils';
 import { createMenuPage } from '../Menu/Menu.render';
 
-export const renderMain = (): void => {
+export const renderMain = (link: string): HTMLElement => {
   const mainEl: HTMLElement = createHtmlElement('main', 'main');
-  document.body.appendChild(mainEl);
+  mainEl.id = 'main';
 
-  mainEl.appendChild(createMenuPage());
+  switch (link) {
+    case '/test-1':
+      mainEl.append('<div>HELLO WORLD</div>');
+      break;
+    case '/test-2':
+      mainEl.append('<div>TEST PAGE</div>');
+      break;
+    default:
+      mainEl.appendChild(createMenuPage());
+  }
+
+  return mainEl;
 };
--- client/src/index.ts
+++ client/src/index.ts
@@ -1,10 +1,65 @@
+/* eslint-disable @typescript-eslint/dot-notation */
+/* eslint-disable prefer-destructuring */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable consistent-return */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import UniversalRouter from 'universal-router';
+import generateUrls from 'universal-router/generateUrls';
+
 import test from '@/services/gameSockets.services';
 import { renderMain } from '@/components/Main/Main.render';
-import './styles/styles.scss';
 import { store } from './redux/store/rootStore';
 
-renderMain();
-test();
+import './styles/styles.scss';
+import { createHtmlElement } from './utils/utils';
+
+const routes = [
+  { name: 'home', path: '/home', action: () => renderMain('default') },
+  { name: 'test-page', path: '/home/test-page', action: () => renderMain('/test-1') },
+  { name: 'test-page-hoba', path: '/home/test-page/hoba', action: () => renderMain('/test-2') },
+];
+
+const router = new UniversalRouter(routes);
+
+const url = generateUrls(router);
+
+const homePage = router.resolve({ pathname: url('home') }).then(res => res);
+homePage.then(res => document.body.append(res!));
+const testPage = router.resolve({ pathname: url('test-page') }).then(res => res);
+const hoba = router.resolve({ pathname: url('test-page-hoba') }).then(res => res);
+
+const oldMain = document.body.lastChild;
+
+const linkHome = document.createElement('a');
+linkHome.setAttribute('href', '#/home');
+linkHome.append('HOME PAGE');
+linkHome.addEventListener('click', () => {
+  oldMain?.remove();
+  homePage.then(res => document.body.append(res!));
+});
+
+const linkTest = document.createElement('a');
+linkTest.setAttribute('href', '#/home/test-page');
+linkTest.append('TEST PAGE');
+linkTest.addEventListener('click', () => {
+  oldMain?.remove();
+  testPage.then(res => document.body.append(res!));
+});
+
+const linkHoba = document.createElement('a');
+linkHoba.setAttribute('href', '#/home/test-page/hoba');
+linkHoba.append('HOBA');
+linkHoba.addEventListener('click', () => {
+  oldMain?.remove();
+  hoba.then(res => document.body.append(res!));
+});
+
+const navBar = createHtmlElement('div', 'nav-bar');
+navBar.append(linkHome, linkTest, linkHoba);
+document.body.append(navBar);
 
 store.subscribe(() => {
   const state = store.getState();
