534b71061372d37ecbb6cde8a62bb344b3762f6d
Arcuman <46977740+Arcuman@users.noreply.github.com>
2021-01-23 13:51:34 +0300

    feat: add init draw of card and avatar

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,27 +1,39 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+
 import cors from 'cors';
+import passport from 'passport';
+import { decksRouter } from '@/resources/decks/decks.router';
+import { cardRouter } from '@/resources/card/card.router';
 import swaggerUI from 'swagger-ui-express';
 import path from 'path';
 import YAML from 'yamljs';
-import passport from 'passport';
-import  {router} from './resources/users/user.router';
+import { returnError, ErrorHandler } from '@/helpers/errorHandler';
 import { authRouter, authenticate } from './resources/auth/auth.router';
+import { router } from './resources/users/user.router';
 
 const app = express();
-
-app.use(express.json());
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
+app.use(express.json());
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.use(passport.initialize());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept,Authorization,Origin');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type, Accept,Authorization,Origin',
+  );
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
-app.use(cors({credentials: true, origin: ['http://localhost:8080', 'http://localhost:8081', 'http://localhost:8082']}));
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:8080', 'http://localhost:8081', 'http://localhost:8082'],
+  }),
+);
 app.options('*', cors());
 
 app.use('/', (req, res, next) => {
@@ -33,9 +45,14 @@ app.use('/', (req, res, next) => {
 });
 
 app.use('/users', authenticate, router);
+app.use('/decks', authenticate, decksRouter);
+app.use('/cards', authenticate, cardRouter);
 app.use('/login', authRouter);
 app.use('/logout', authRouter);
 app.use('/register', authRouter);
 app.use('/refresh-tokens', authRouter);
-
+app.use((err: ErrorHandler, req: Request, res: Response, next: NextFunction) => {
+  returnError(err, res);
+  next();
+});
 export default app;
--- server/src/resources/game/game.controller.ts
+++ server/src/resources/game/game.controller.ts
@@ -1,37 +1,44 @@
-import {Server, Socket} from 'socket.io';
-import {closeSocket, sendInitState} from '@/resources/game/game.service';
-import {createPlayer } from '@/resources/game/player/player.service';
-import {Room} from '@/resources/game/room/room.model';
-import {closeRoom, getOrCreateOpenRoom} from '@/resources/game/room/room.service';
+import { Server, Socket } from 'socket.io';
+import { closeSocket, sendInitState } from '@/resources/game/game.service';
+import { createPlayer } from '@/resources/game/player/player.service';
+import { Room } from '@/resources/game/room/room.model';
+import { closeRoom, getOrCreateOpenRoom } from '@/resources/game/room/room.service';
 import {
   ALREADY_PLAY,
-  CLOSE_SOCKET, DISCONNECT,
+  CLOSE_SOCKET,
+  DISCONNECT,
   HAND_CARD_PLAY,
   NEXT_TURN,
   OPPONENT_FOUND,
-  START_GAME, TABLE_CARD_PLAY_CARD_TARGET, TABLE_CARD_PLAY_PLAYER_TARGET,
+  START_GAME,
+  TABLE_CARD_PLAY_CARD_TARGET,
+  TABLE_CARD_PLAY_PLAYER_TARGET,
   WAIT_SECOND_PLAYER,
 } from '@/resources/game/constants';
-import {Card} from '@/resources/card/card.models';
-import {nextTurn} from '@/resources/game/servicies/nextTurn.service';
-import {countDownTimer} from '@/resources/game/servicies/timer.service';
-import {handCardPlay} from '@/resources/game/servicies/handCardPlay.service';
-import {tableCardPlayTargetPlayer} from '@/resources/game/servicies/tableCardPlayTargerPlayer.service';
-import {tableCardPlayTargetCard} from '@/resources/game/servicies/tableCardPlayTargerCard.service';
-import {webToken} from '@/helpers/webToken';
+import { Card } from '@/resources/card/card.model';
+import { nextTurn } from '@/resources/game/servicies/nextTurn.service';
+import { countDownTimer } from '@/resources/game/servicies/timer.service';
+import { handCardPlay } from '@/resources/game/servicies/handCardPlay.service';
+import { tableCardPlayTargetPlayer } from '@/resources/game/servicies/tableCardPlayTargerPlayer.service';
+import { tableCardPlayTargetCard } from '@/resources/game/servicies/tableCardPlayTargerCard.service';
+import { webToken } from '@/helpers/webToken';
 
-function isPlayerPlayed(rooms: Array<Room>, userId:number):boolean{
-  return rooms.some( room => room.players.some((player)=> player.userId === userId));
+function isPlayerPlayed(rooms: Array<Room>, userId: number): boolean {
+  return rooms.some(room => room.players.some(player => player.userId === userId));
 }
 
-export default async function gameLogic(io: Server, socket: Socket, rooms: Array<Room>): Promise<void> {
+export default async function gameLogic(
+  io: Server,
+  socket: Socket,
+  rooms: Array<Room>,
+): Promise<void> {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore.
   const userId = webToken.getDataFromToken(socket.handshake.query.token);
   if (isPlayerPlayed(rooms, userId)) {
     socket.emit(ALREADY_PLAY);
     socket.disconnect();
-    return ;
+    return;
   }
   const player = await createPlayer(userId, socket);
   const openRoom: Room = getOrCreateOpenRoom(rooms);
@@ -48,23 +55,21 @@ export default async function gameLogic(io: Server, socket: Socket, rooms: Array
     io.to(openRoom.id).emit(WAIT_SECOND_PLAYER);
   }
 
-  player.socket.on(NEXT_TURN, ()=> nextTurn(openRoom, player, io));
+  player.socket.on(NEXT_TURN, () => nextTurn(openRoom, player, io));
 
-  player.socket.on(HAND_CARD_PLAY, (card: Card)=>{
+  player.socket.on(HAND_CARD_PLAY, (card: Card) => {
     handCardPlay(card, player, openRoom, io);
   });
 
-  player.socket.on(TABLE_CARD_PLAY_PLAYER_TARGET, (cardId:number)=>{
+  player.socket.on(TABLE_CARD_PLAY_PLAYER_TARGET, (cardId: number) => {
     tableCardPlayTargetPlayer(cardId, openRoom, player, io);
   });
-  player.socket.on(TABLE_CARD_PLAY_CARD_TARGET, (cardId:number, targetId:number)=>{
+  player.socket.on(TABLE_CARD_PLAY_CARD_TARGET, (cardId: number, targetId: number) => {
     tableCardPlayTargetCard(cardId, targetId, openRoom, player, io);
   });
 
-  player.socket.on(CLOSE_SOCKET
-    , () => closeSocket(openRoom, rooms, player));
-  player.socket.on(DISCONNECT
-    , () => {
-      closeRoom(openRoom, rooms);
-    });
+  player.socket.on(CLOSE_SOCKET, () => closeSocket(openRoom, rooms, player));
+  player.socket.on(DISCONNECT, () => {
+    closeRoom(openRoom, rooms);
+  });
 }
