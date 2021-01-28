aabf9d27aab8a5ee3c478891025b686a45190e00
halinapp <galunikipp@gmail.com>
2021-01-28 05:27:28 +0300

    feat:add audio bg files, in menu, my cards

---
+++ client/src/components/Game/audio.constants.ts
@@ -0,0 +1,38 @@
+export const AUDIO = {
+  MENU_BG_AUDIO: {
+    NAME: 'menu-background-audio',
+    PATH: 'assets/audio/background/menu_bga.mp3',
+  },
+  PROFILE_BG_AUDIO: {
+    NAME: 'profile-background-audio',
+    PATH: 'assets/audio/background/profile_bga.ogg',
+  },
+  MYCARDS_BG_AUDIO: {
+    NAME: 'mycards-background-audio',
+    PATH: 'assets/audio/background/mycards_bga.mp3',
+  },
+  PLAYER_TURN_BG_AUDIO: {
+    NAME: 'player-turn-background-audio',
+    PATH: 'assets/audio/background/player_turn_bga.mp3',
+  },
+  ENEMY_TURN_BG_AUDIO: {
+    NAME: 'enemy-turn-background-audio',
+    PATH: 'assets/audio/background/enemy_turn_bga.ogg',
+  },
+  BUTTON_CLICK_AUDIO:{
+    NAME: 'button-press-audio',
+    PATH: 'assets/audio/button_press.ogg',
+  },
+  BUTTON_OVER_AUDIO:{
+    NAME: 'button-over-audio',
+    PATH: 'assets/audio/button_mouse_over.ogg',
+  },
+  CARD_OVER_AUDIO:{
+    NAME: 'card-over-audio',
+    PATH: 'assets/audio/card_mouse_over.ogg',
+  },
+  CARD_AWAY_AUDIO:{
+    NAME: 'card-away-audio',
+    PATH: 'assets/audio/card_mouse_away.ogg',
+  },
+};
