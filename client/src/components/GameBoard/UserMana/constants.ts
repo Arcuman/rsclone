2d0240231aba8d029cb47ba53d83c0dadc3f2aa4
Ilya-Baklanov <yahters4@gmail.com>
2021-02-03 03:58:41 +0400

    fix: constants for user-mana background

--- client/src/components/GameBoard/UserMana/constants.ts
+++ client/src/components/GameBoard/UserMana/constants.ts
@@ -3,3 +3,4 @@ export const PLAYER_MANA_X = 220;
 export const PLAYER_MANA_Y = 650;
 export const BACKGROUND_USER_MANA_X = 220;
 export const BACKGROUND_USER_MANA_Y = 650;
+export const BACKGROUND_NAME_USER_MANA = 'user_mana';
